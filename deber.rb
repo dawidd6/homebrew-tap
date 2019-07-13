072715d1f440d1647b5d1539461a9329722404cf
Dawid Dziurla <dawidd0811@gmail.com>
2019-07-14 01:48:33 +0200

    deber: delete GOPATH + add head

--- deber.rb
+++ deber.rb
@@ -2,12 +2,12 @@ class Deber < Formula
   desc "Debian packaging with Docker"
   homepage "https://github.com/dawidd6/deber"
   version "0.5"
+  head "#{homepage}.git"
   url "#{homepage}/archive/#{version}.tar.gz"
   sha256 "77838bdcbcb4b37a395314d3679a2a9fd0fd0c7909cee3a277bc50c8816b8839"
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = "#{buildpath}/go"
     system "go", "build", "-v", "-o", "#{bin}/#{name}"
   end
 
