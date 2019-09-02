534d14616686bb09eef3038aacf2372e04054e08
Dawid Dziurla <dawidd0811@gmail.com>
2019-09-02 17:07:38 +0200

    add neofetch

---
+++ neofetch.rb
@@ -0,0 +1,16 @@
+class Neofetch < Formula
+  desc "Fast, highly customisable system info script"
+  homepage "https://github.com/dylanaraps/neofetch"
+  url "https://github.com/dylanaraps/neofetch/archive/6.1.0.tar.gz"
+  sha256 "ece351e35286b64d362000d409b27597fcbdcf77e8e60fa0adae1f29d3c29637"
+  head "https://github.com/dylanaraps/neofetch.git"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/neofetch", "--config", "none", "--color_blocks", "off",
+                              "--disable", "wm", "de", "term", "gpu"
+  end
+end
