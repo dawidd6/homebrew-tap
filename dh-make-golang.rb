f1ec725bf4647fc78800373547dd3d9cfe03155d
Dawid Dziurla <dawidd0811@gmail.com>
2019-09-13 16:42:10 +0200

    dh-make-golang: improve

--- dh-make-golang.rb
+++ dh-make-golang.rb
@@ -1,15 +1,17 @@
 class DhMakeGolang < Formula
   desc "Automatically creates Debian packaging for Go packages"
   homepage "https://github.com/Debian/dh-make-golang"
-  head "https://github.com/dawidd6/dh-make-golang.git"
+  url "https://github.com/dawidd6/dh-make-golang.git",
+    :tag     => "v19.09.13",
+    :revison => "420dcab5daa95a7919bded78e297706f20a25b59"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/"#{name}"
+    system "go", "build", "-o", bin/name
   end
 
   test do
-    system "true"
+    system bin/name, "search", "wuzz"
   end
 end
