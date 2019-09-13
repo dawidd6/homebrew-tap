e751142cf099feaa3298ff63391f68f4e5adc8c5
Dawid Dziurla <dawidd0811@gmail.com>
2019-09-13 12:56:16 +0200

    add keybase-cli

---
+++ keybase-cli.rb
@@ -0,0 +1,18 @@
+class KeybaseCli < Formula
+  desc "Command-line interface to Keybase.io"
+  homepage "https://keybase.io"
+  url "https://github.com/keybase/client/archive/v4.4.1.tar.gz"
+  sha256 "dfac5ff4ecd5cd75f22ebece24c2e074cc83e38c7b207e074ca2c96ec3c96b7b"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/keybase/client/").install "go"
+    system "go", "build", "-tags", "production", "-o", bin/"keybase", "github.com/keybase/client/go/keybase"
+  end
+
+  test do
+    system bin/"keybase", "--standalone", "id", "homebrew"
+  end
+end
