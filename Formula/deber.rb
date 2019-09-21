db8f32825c40692c95fe141ff90cb9484a656ad1
Dawid Dziurla <dawidd0811@gmail.com>
2019-09-21 19:21:40 +0200

    satisfy audit

--- Formula/deber.rb
+++ Formula/deber.rb
@@ -1,9 +1,9 @@
 class Deber < Formula
   desc "Debian packaging with Docker"
   homepage "https://github.com/dawidd6/deber"
-  head "#{homepage}.git"
   url "https://github.com/dawidd6/deber/archive/0.6.2.tar.gz"
   sha256 "1c1e75bf36b6a2b412faec793f904f09b65c6e1ebf00f718096ea6a6698be359"
+  head "#{homepage}.git"
 
   depends_on "go" => :build
 
