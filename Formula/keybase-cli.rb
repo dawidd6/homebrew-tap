268aecb923e3faf48fdda2d1f304a53c4bcb8755
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-22 18:50:34 +0000

    keybase-cli: add 4.4.1 bottle.

--- Formula/keybase-cli.rb
+++ Formula/keybase-cli.rb
@@ -4,6 +4,12 @@ class KeybaseCli < Formula
   url "https://github.com/keybase/client/archive/v4.4.1.tar.gz"
   sha256 "dfac5ff4ecd5cd75f22ebece24c2e074cc83e38c7b207e074ca2c96ec3c96b7b"
 
+  bottle do
+    root_url "https://dl.bintray.com/dawidd6/bottles-tap"
+    cellar :any_skip_relocation
+    sha256 "7d9941db13c89b62aee8aba00ddf84dcaa2b94bfcdd24e7a139ade9600e59509" => :x86_64_linux
+  end
+
   depends_on "go" => :build
 
   def install
