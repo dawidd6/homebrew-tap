5e579bb4f05615c7e9b4b717f390205e1c06e07e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-22 16:52:20 +0000

    deber: add 0.6.2 bottle.

--- Formula/deber.rb
+++ Formula/deber.rb
@@ -6,6 +6,12 @@ class Deber < Formula
   sha256 "1c1e75bf36b6a2b412faec793f904f09b65c6e1ebf00f718096ea6a6698be359"
   head "#{homepage}.git"
 
+  bottle do
+    root_url "https://dl.bintray.com/dawidd6/bottles-tap"
+    cellar :any_skip_relocation
+    sha256 "e563b2f8094b998e0b221f911b1816b9a8b321af00eee74272679474c423aa7f" => :x86_64_linux
+  end
+
   depends_on "go" => :build
 
   def install
