cb798019178cdd0bb1381c7fe1139c65e8382880
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-22 17:37:56 +0000

    dh-make-golang: add 19.09.13 bottle.

--- Formula/dh-make-golang.rb
+++ Formula/dh-make-golang.rb
@@ -6,6 +6,12 @@ class DhMakeGolang < Formula
     :tag     => "v19.09.13",
     :revison => "420dcab5daa95a7919bded78e297706f20a25b59"
 
+  bottle do
+    root_url "https://dl.bintray.com/dawidd6/bottles-tap"
+    cellar :any_skip_relocation
+    sha256 "13729000eda823e3bd73dc3059aeb58603dcb08e7d6239909f89ced55cc1869e" => :x86_64_linux
+  end
+
   depends_on "go" => :build
 
   def install
