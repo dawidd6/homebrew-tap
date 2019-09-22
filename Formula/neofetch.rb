9b4bfa6347c644d5039367bb014e83ae77e8c3e6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-22 15:37:33 +0000

    neofetch: add 6.1.0 bottle.

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -5,6 +5,12 @@ class Neofetch < Formula
   sha256 "ece351e35286b64d362000d409b27597fcbdcf77e8e60fa0adae1f29d3c29637"
   head "https://github.com/dylanaraps/neofetch.git"
 
+  bottle do
+    root_url "https://dl.bintray.com/dawidd6/bottles-tap"
+    cellar :any_skip_relocation
+    sha256 "9a378b8d63a93c6cc05f93d05eee1400575e893793ec19740cf02e1e4e0d6eed" => :x86_64_linux
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
