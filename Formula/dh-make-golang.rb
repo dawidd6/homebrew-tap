a10fb80f1d3ea99068d6237bc4cd09852a3d6fcf
Dawid Dziurla <dawidd0811@gmail.com>
2019-10-01 16:40:19 +0200

    dh-make-golang: delete prefix from bottle block

--- Formula/dh-make-golang.rb
+++ Formula/dh-make-golang.rb
@@ -7,7 +7,6 @@ class DhMakeGolang < Formula
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
-    prefix "/home/linuxbrew/.linuxbrew"
     cellar :any_skip_relocation
     rebuild 1
     sha256 "4f1c4b8576690a12f2ae435ccd6c0f1c90feb796783b049a40b34d3b50585792" => :mojave
