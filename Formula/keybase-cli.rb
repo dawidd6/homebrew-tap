bb325938347816567dacbc2977231c48327e8efd
Dawid Dziurla <dawidd0811@gmail.com>
2019-10-12 23:39:09 +0200

    keybase-cli 4.6.0
    Closes #52.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/keybase-cli.rb
+++ Formula/keybase-cli.rb
@@ -1,8 +1,8 @@
 class KeybaseCli < Formula
   desc "Command-line interface to Keybase.io"
   homepage "https://keybase.io"
-  url "https://github.com/keybase/client/archive/v4.4.1.tar.gz"
-  sha256 "dfac5ff4ecd5cd75f22ebece24c2e074cc83e38c7b207e074ca2c96ec3c96b7b"
+  url "https://github.com/keybase/client/archive/v4.6.0.tar.gz"
+  sha256 "ee97b78e27dc77f55e0533dfbf4ecde2563c14e0f8c708cdb6066361c56ff8f8"
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
