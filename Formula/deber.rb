7d2c6fc04fa2fca12d81e2f439223e1950a2d027
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-07 10:08:35 +0100

    deber: catalina
    Closes #60.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/deber.rb
+++ Formula/deber.rb
@@ -4,6 +4,7 @@ class Deber < Formula
   url "https://github.com/dawidd6/deber/archive/0.6.2.tar.gz"
   sha256 "1c1e75bf36b6a2b412faec793f904f09b65c6e1ebf00f718096ea6a6698be359"
   head "#{homepage}.git"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
