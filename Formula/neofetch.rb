f2942c7014b874db92a437d2e19670bd0d4e5d48
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-07 10:14:45 +0100

    neofetch: catalina
    Closes #64.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -4,6 +4,7 @@ class Neofetch < Formula
   url "https://github.com/dylanaraps/neofetch/archive/6.1.0.tar.gz"
   sha256 "ece351e35286b64d362000d409b27597fcbdcf77e8e60fa0adae1f29d3c29637"
   head "https://github.com/dylanaraps/neofetch.git"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
