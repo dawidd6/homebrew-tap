91755bfd2abcee3c657e99ea7f9e827c05657ed6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-12 16:40:26 +0000

    actions-updater: update 0.1.10 bottle.

--- Formula/actions-updater.rb
+++ Formula/actions-updater.rb
@@ -8,8 +8,8 @@ class ActionsUpdater < Formula
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
     cellar :any_skip_relocation
-    sha256 "da42e75878654d9f5f4df5fe0fdaa57bdac5f88c5ccbd1cb0ae9b8238e08bd7a" => :catalina
-    sha256 "62e41afa4977a9e2af47b297e07684422855b8fe3193b3d44a56684a5ddb3adb" => :x86_64_linux
+    sha256 "cc64a987d921d6467c388c00d6033d1a24df18abdce588447c6e30b7cb76d2fa" => :catalina
+    sha256 "602984ad54120156f8406cda4302bbd353e318ddc7b57f6032a51cbdc08c8be4" => :x86_64_linux
   end
 
   uses_from_macos "ruby"
