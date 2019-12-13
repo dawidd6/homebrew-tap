e45aa76cb1d0c4a361c8aa07a780196b9f2ca00c
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-13 12:17:16 +0100

    actions-updater 0.1.11

--- Formula/actions-updater.rb
+++ Formula/actions-updater.rb
@@ -1,8 +1,8 @@
 class ActionsUpdater < Formula
   desc "Updater of used Github Actions in workflow files"
   homepage "https://github.com/dawidd6/actions-updater"
-  url "https://github.com/dawidd6/actions-updater/archive/v0.1.10.tar.gz"
-  sha256 "704cd7b21646a2031fd98d709a287f2c435f3ac65ef9e62f0009d982d74277fe"
+  url "https://github.com/dawidd6/actions-updater/archive/v0.1.11.tar.gz"
+  sha256 "b1c83ee9d19289eb403ad0863c235fa9c3b3a980c9b13a43cda9fc9413935df4"
   head "https://github.com/dawidd6/actions-updater.git"
 
   bottle do
