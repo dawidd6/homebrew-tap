1dc9ac5a6ee1f8543907cd874429f5ed518ab517
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-14 23:41:31 +0100

    forgit HEAD (new formula)

---
+++ Formula/forgit.rb
@@ -0,0 +1,24 @@
+class Forgit < Formula
+  desc "A utility tool powered by fzf for using git interactively"
+  homepage "https://github.com/wfxr/forgit.git"
+  head "https://github.com/wfxr/forgit.git"
+
+  bottle :unneeded
+
+  depends_on "fzf"
+
+  def install
+    share.install Dir["forgit*"]
+  end
+
+  def caveats
+    <<~EOS
+      Place this line in shell config file (choose extension wisely):
+        source #{share}/forgit.plugin.{sh,zsh,fish}
+    EOS
+  end
+
+  test do
+    system "bash", "-c", "source #{share}/forgit.plugin.sh && alias ga"
+  end
+end
