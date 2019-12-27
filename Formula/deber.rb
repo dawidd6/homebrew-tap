98998c322b415bf0cdfefc6ace031c769666c143
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-27 13:49:02 +0100

    deber 1.0.0
    Closes #81.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/deber.rb
+++ Formula/deber.rb
@@ -1,10 +1,9 @@
 class Deber < Formula
   desc "Debian packaging with Docker"
   homepage "https://github.com/dawidd6/deber"
-  url "https://github.com/dawidd6/deber/archive/0.6.2.tar.gz"
-  sha256 "1c1e75bf36b6a2b412faec793f904f09b65c6e1ebf00f718096ea6a6698be359"
+  url "https://github.com/dawidd6/deber/archive/v1.0.0.tar.gz"
+  sha256 "924d8dd9c12514c6c27f75f2ed64b222d3db84383de240c3eaefb22f9ca7e779"
   head "#{homepage}.git"
-  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
