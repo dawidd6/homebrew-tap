85b6d76dfcff8c1f1110d687f687adc408b0c36d
Dawid Dziurla <dawidd0811@gmail.com>
2020-01-19 12:34:15 +0100

    git-buildpackage: improve
    - depend on `dpkg` only for build and test
    - update pypi packages

--- Formula/git-buildpackage.rb
+++ Formula/git-buildpackage.rb
@@ -6,7 +6,7 @@ class GitBuildpackage < Formula
   url "https://github.com/agx/git-buildpackage.git",
       :tag      => "debian/0.9.17",
       :revision => "d4ca59b3d9a2be8657b08b1073b44b68b2d55742"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
@@ -15,18 +15,18 @@ class GitBuildpackage < Formula
     sha256 "6c555b08e64a339af542ddc3267bd1544c8301e5fef00ac9e44143af27157f9a" => :x86_64_linux
   end
 
-  depends_on "dpkg"
+  depends_on "dpkg" => [:build, :test]
   depends_on "pristine-tar"
   depends_on "python"
 
   resource "dateutil" do
-    url "https://files.pythonhosted.org/packages/ad/99/5b2e99737edeb28c71bcbec5b5dda19d0d9ef3ca3e92e3e925e7c0bb364c/python-dateutil-2.8.0.tar.gz"
-    sha256 "c89805f6f4d64db21ed966fda138f8a5ed7a4fdbc1a8ee329ce1b74e3c74da9e"
+    url "https://files.pythonhosted.org/packages/be/ed/5bbc91f03fa4c839c4c7360375da77f9659af5f7086b7a7bdda65771c8e0/python-dateutil-2.8.1.tar.gz"
+    sha256 "73ebfe9dbf22e832286dafa60473e4cd239f8592f699aa5adaf10050e6e1823c"
   end
 
   resource "six" do
-    url "https://files.pythonhosted.org/packages/dd/bf/4138e7bfb757de47d1f4b6994648ec67a51efe58fa907c1e11e350cddfca/six-1.12.0.tar.gz"
-    sha256 "d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73"
+    url "https://files.pythonhosted.org/packages/21/9f/b251f7f8a76dec1d6651be194dfba8fb8d7781d10ab3987190de8391d08e/six-1.14.0.tar.gz"
+    sha256 "236bdbdce46e6e6a3d61a337c0f8b763ca1e8717c03b369e87a7ec7ce1319c0a"
   end
 
   def install
