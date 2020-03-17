49cc17d3bb3b997588aad6f98b2df6b7bccffd8c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-17 20:06:45 +0000

    clone-org: add 1.1.1 bottle.

--- Formula/clone-org.rb
+++ Formula/clone-org.rb
@@ -5,6 +5,13 @@ class CloneOrg < Formula
   sha256 "f925d286e154566a29e8b12580a2dbb127de205d03f1f8980524f21da2fcffc9"
   head "https://github.com/caarlos0/clone-org.git"
 
+  bottle do
+    root_url "https://dl.bintray.com/dawidd6/bottles-tap"
+    cellar :any_skip_relocation
+    sha256 "dde1314730bc6823f726372ede289ff435d2f7d5a82e4365c34396aac2e601ae" => :catalina
+    sha256 "d636f98f6880f5980f1832e5310ac63d72c9e40977dd3a58f29f562ffd85c7e6" => :x86_64_linux
+  end
+
   depends_on "go" => :build
 
   def install
