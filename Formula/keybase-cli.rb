6ad3ea4f1a9d6c5e4cc423a30f7a57fa279f037a
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-23 19:42:36 +0100

    keybase-cli 5.3.1

--- Formula/keybase-cli.rb
+++ Formula/keybase-cli.rb
@@ -1,8 +1,8 @@
 class KeybaseCli < Formula
   desc "Command-line interface to Keybase.io"
   homepage "https://keybase.io"
-  url "https://github.com/keybase/client/archive/v5.2.1.tar.gz"
-  sha256 "209860c9b8c8e7d24b93b53afa2865aea543c2b83a3110f822d54b569f4deffe"
+  url "https://github.com/keybase/client/archive/v5.3.1.tar.gz"
+  sha256 "6bff8f3e892bcfda21a608ae1c38f8d5428882b517f61e2dc429259ff07e30cb"
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
