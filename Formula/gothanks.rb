ff94bf2e6462adae0933486b56e335cb77be508b
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-31 16:34:13 +0200

    gothanks: bump revision

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -3,6 +3,7 @@ class Gothanks < Formula
   homepage "https://github.com/psampaz/gothanks"
   url "https://github.com/psampaz/gothanks/archive/v0.2.0.tar.gz"
   sha256 "5ebb888cdf4b73139fb7cd27787a95e9e31cd15a2e8f51992be8a1bff6c0fcda"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
@@ -19,6 +20,6 @@ class Gothanks < Formula
 
   test do
     ENV.delete "GITHUB_TOKEN"
-    assert_match "no Github token found", shell_output("#{bin/name}", 255)
+    assert_match "no Github token found", shell_output(bin/"gothanks", 255)
   end
 end
