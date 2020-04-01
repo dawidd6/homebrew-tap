88ece7506f1273bf567f543e29d29bedff78612e
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-01 19:27:49 +0200

    upload: send name and email in payload

--- upload.sh
+++ upload.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eu
 
-curl --data "{\"event_type\":\"upload\",\"client_payload\":{\"pr\": $1}}" \
+user="$(git config --get user.name)"
+email="$(git config --get user.email)"
+
+curl --data "{\"event_type\":\"upload\",\"client_payload\":{\"pr\": $1,\"name\":\"$user\",\"email\":\"$email\"}}" \
      --header "Authorization: token $HOMEBREW_GITHUB_API_TOKEN" \
      'https://api.github.com/repos/dawidd6/homebrew-tap/dispatches'
 
