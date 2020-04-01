2ff6ea40303a037bb647eff5e55b75faaef1acff
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-01 19:22:09 +0200

    upload: fix and chmod +x

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-curl --data '{"event_type":"upload","client_payload":{"pr": $1}}' \
+curl --data "{\"event_type\":\"upload\",\"client_payload\":{\"pr\": $1}}" \
      --header "Authorization: token $HOMEBREW_GITHUB_API_TOKEN" \
      'https://api.github.com/repos/dawidd6/homebrew-tap/dispatches'
 
