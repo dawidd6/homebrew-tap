b4c3fb16e34390ebb984e94b1040a6ebfa768e84
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-05 09:19:02 +0000

    ameba: update 0.12.0 bottle.

--- Formula/ameba.rb
+++ Formula/ameba.rb
@@ -7,8 +7,8 @@ class Ameba < Formula
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
     cellar :any_skip_relocation
-    sha256 "0e623718d42cabc855bcbbd1f24ecc8dc2163235f73019a9193e4a4648d68b37" => :catalina
-    sha256 "5643a3c26172eb06416a10e86c6da6e994b72cff3be211794b767353db6d690c" => :x86_64_linux
+    sha256 "662ab9d6e836a07228bd4ca24c90211f0fb8ea2362a8472ec77ac9375b27e898" => :catalina
+    sha256 "31789fae4d0e03c5646ce0ce3092ef77adbe4ff6c3acf2d2b13d6c2dd8e019c7" => :x86_64_linux
   end
 
   depends_on "crystal" => :build
