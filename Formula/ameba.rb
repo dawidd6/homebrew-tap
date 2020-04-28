3ae3674abdf8f60b997cb9f4caf4f99dcb67a10f
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-28 15:42:03 +0000

    ameba 0.12.1

--- Formula/ameba.rb
+++ Formula/ameba.rb
@@ -1,8 +1,8 @@
 class Ameba < Formula
   desc "Static code analysis tool for Crystal"
   homepage "https://crystal-ameba.github.io"
-  url "https://github.com/crystal-ameba/ameba/archive/v0.12.0.tar.gz"
-  sha256 "1709db9de850430b38b38998fa38bf70c237925c6072bf78dfa51ad66cabdb57"
+  url "https://github.com/crystal-ameba/ameba/archive/v0.12.1.tar.gz"
+  sha256 "23ae1671cde9d32ebaeffe677ef5392c2e4c82c9cc5467b8954796898f8fd88d"
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
