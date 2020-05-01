66b54d95634a20bd0a67fa4fe1648f00ed43aede
Dawid Dziurla <dawidd0811@gmail.com>
2020-05-01 19:11:14 +0200

    actions-updater: revision bump

--- Formula/actions-updater.rb
+++ Formula/actions-updater.rb
@@ -3,6 +3,7 @@ class ActionsUpdater < Formula
   homepage "https://github.com/dawidd6/actions-updater"
   url "https://github.com/dawidd6/actions-updater/archive/v0.1.15.tar.gz"
   sha256 "e415ab6bb8547d2571d1dd80c686531d21f191e8f080f351355891b1bf96075f"
+  revision 1
   head "https://github.com/dawidd6/actions-updater.git"
 
   bottle do
