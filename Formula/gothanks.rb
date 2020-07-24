4f45b5c1594b268594404503e0ef90212640ac7a
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-24 18:28:43 +0200

    gothanks: update
    Closes #156.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -3,6 +3,7 @@ class Gothanks < Formula
   homepage "https://github.com/psampaz/gothanks"
   url "https://github.com/psampaz/gothanks/archive/v0.3.0.tar.gz"
   sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
+  license "MIT"
   revision 1
 
   bottle do
@@ -15,7 +16,7 @@ class Gothanks < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/name
+    system "go", "build", *std_go_args
   end
 
   test do
