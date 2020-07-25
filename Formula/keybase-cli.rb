77dd02252d21a26a1293815d0bda8070b08ac1ef
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-25 20:08:50 +0200

    keybase-cli: add license
    Closes #162.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/keybase-cli.rb
+++ Formula/keybase-cli.rb
@@ -3,6 +3,7 @@ class KeybaseCli < Formula
   homepage "https://keybase.io"
   url "https://github.com/keybase/client/archive/v5.5.1.tar.gz"
   sha256 "a65dc4b62fc1299dd17da52ddd2484fa1dc1e7d2a4776c3a6e112ee020980b12"
+  license "BSD-3-Clause"
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
