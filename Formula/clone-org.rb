6e88d6a81b3b46035b1887732b45f3d1e812b8dd
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-20 12:13:13 +0000

    clone-org 1.2.0
    Closes #168.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/clone-org.rb
+++ Formula/clone-org.rb
@@ -1,8 +1,8 @@
 class CloneOrg < Formula
   desc "Clone all repos of a github organization"
   homepage "https://github.com/caarlos0/clone-org"
-  url "https://github.com/caarlos0/clone-org/archive/v1.1.2.tar.gz"
-  sha256 "5d8ffd94da079ea4eec57ed3b11977edb316ee1e510bbd4bdc4915905b8d8dd1"
+  url "https://github.com/caarlos0/clone-org/archive/v1.2.0.tar.gz"
+  sha256 "930c1c71da669d45a9e8e006a1f2d5782c268bc35443bb7e648b7d1690936026"
   license "MIT"
   head "https://github.com/caarlos0/clone-org.git"
 
