5d87a9454b3261746fdd4a8fdbe5e2fd070073a7
Dawid Dziurla <dawidd0811@gmail.com>
2020-09-15 18:35:32 +0200

    devscripts: revision bump

--- Formula/devscripts.rb
+++ Formula/devscripts.rb
@@ -5,6 +5,7 @@ class Devscripts < Formula
     tag:      "v2.20.4",
     revision: "c06307c0b109d05784a6b5990bb08286f00af9c7"
   license "GPL-2.0-only"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/dawidd6/bottles-tap"
