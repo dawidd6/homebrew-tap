1e7c9414b4f145db95a71bd1aa9df5099f2e3814
Dawid Dziurla <dawidd0811@gmail.com>
2020-09-18 21:53:50 +0200

    go@1: case insensitive regex

--- Formula/go@1.rb
+++ Formula/go@1.rb
@@ -8,7 +8,7 @@ class GoAT1 < Formula
 
   livecheck do
     url "https://golang.org/dl"
-    regex(/go(([0-9.]+)((beta|rc|alpha)[1-9])?)\.src/)
+    regex(/go(([0-9.]+)((beta|rc|alpha)[1-9])?)\.src/i)
   end
 
   bottle do
