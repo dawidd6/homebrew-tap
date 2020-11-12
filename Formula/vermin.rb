cecb8184fbf505e2e9c4f5298ce2b5335b761e50
Dawid Dziurla <dawidd0811@gmail.com>
2020-11-12 18:51:00 +0000

    vermin: add 0.109.0 bottle.

--- Formula/vermin.rb
+++ Formula/vermin.rb
@@ -6,6 +6,13 @@ class Vermin < Formula
   license "Apache-2.0"
   head "https://github.com/mhewedy/vermin.git"
 
+  bottle do
+    root_url "https://github.com/dawidd6/homebrew-tap/releases/download/vermin-0.109.0"
+    cellar :any_skip_relocation
+    sha256 "74d1242dedc84fec338bf17c03514593150f71f1b510f16e91bb2d827e171bb2" => :catalina
+    sha256 "59cc50f3682a0f758b52675b55059c08ff156d9459d35a4306ef44c42b453b9b" => :x86_64_linux
+  end
+
   depends_on "go" => :build
 
   def install
