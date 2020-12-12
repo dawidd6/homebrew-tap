eb8cff29fe04b0260641de0cf557b8dd0cebf4fb
Dawid Dziurla <dawidd0811@gmail.com>
2020-12-12 22:42:43 +0000

    actions-updater: update 0.1.15_2 bottle.

--- Formula/actions-updater.rb
+++ Formula/actions-updater.rb
@@ -11,6 +11,7 @@ class ActionsUpdater < Formula
     root_url "https://github.com/dawidd6/homebrew-tap/releases/download/actions-updater-0.1.15_2"
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "3d2037149d487bda679aead7446e1f7de22d629cd19012c01c2acffa1df0d220" => :big_sur
     sha256 "8733c684af6c6bb099c7c71c3fd2d9878212476d94e77fa840e8b5d1e2737ccb" => :catalina
     sha256 "ffcad3e22b69dfa60653b6cfb01ec66c5f753d3324d1053b4a13e0bb06febb9b" => :x86_64_linux
   end
