e0a41304cee9ae8070517f4a183c16518ed5c620
Dawid Dziurla <dawidd0811@gmail.com>
2020-12-12 22:44:17 +0000

    ameba: update 0.13.2_1 bottle.

--- Formula/ameba.rb
+++ Formula/ameba.rb
@@ -9,6 +9,7 @@ class Ameba < Formula
   bottle do
     root_url "https://github.com/dawidd6/homebrew-tap/releases/download/ameba-0.13.2_1"
     cellar :any
+    sha256 "3b88814669482d5c6b89fd869a2c476bd431e5ded4b211674428fe53d1f020e6" => :big_sur
     sha256 "fe02e383d1a7480274ec6dab20ef89b163c1b579a9a242c826f731c4c057a565" => :catalina
     sha256 "cea9d9a6d5e156e4b1f2a5763e535443a0994ffd4162d2f0e5c3471c817b1c14" => :x86_64_linux
   end
