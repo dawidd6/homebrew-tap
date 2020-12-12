0f56a0274b7ea9c4f78e4f7ff4e77bf1ca3fb477
Dawid Dziurla <dawidd0811@gmail.com>
2020-12-12 22:52:55 +0000

    keybase-cli: update 5.5.2 bottle.

--- Formula/keybase-cli.rb
+++ Formula/keybase-cli.rb
@@ -8,6 +8,7 @@ class KeybaseCli < Formula
   bottle do
     root_url "https://github.com/dawidd6/homebrew-tap/releases/download/keybase-cli-5.5.2"
     cellar :any_skip_relocation
+    sha256 "15258bb10e019649142ac2a01c4784542ae1dcf5ed2d7e5288d8fdf362276988" => :big_sur
     sha256 "aabcdba144a062657e9ad9c03286544f3e2b729a8c30287f59dfdd715250eb66" => :catalina
     sha256 "a110652139bdc99e3b5440da3fa89a43d0d23074a32007a11fa3715b70088624" => :x86_64_linux
   end
