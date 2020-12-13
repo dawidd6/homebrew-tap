cd2aa8c9f294bd02f92dc57d1eeeb9660591b155
Dawid Dziurla <dawidd0811@gmail.com>
2020-12-13 00:14:10 +0000

    deber: update 1.0.0_1 bottle.

--- Formula/deber.rb
+++ Formula/deber.rb
@@ -10,6 +10,7 @@ class Deber < Formula
   bottle do
     root_url "https://github.com/dawidd6/homebrew-tap/releases/download/deber-1.0.0_1"
     cellar :any_skip_relocation
+    sha256 "4854097d48a1452de3df840be38e8a814f0a05d2a3ffaa175985cfa411e31967" => :big_sur
     sha256 "58861b90fe2d8e84547c5bd415ab6f4b3721553f639bc96ccda13c716c70a9be" => :catalina
     sha256 "54adecadd523a0cd2dbafb0c2fb3a6b902c574a14f2ec845b62655cfa118927e" => :x86_64_linux
   end
