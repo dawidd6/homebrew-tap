b2c4b5f8a998360b253a811a33793e2b1eb51bac
Dawid Dziurla <dawidd0811@gmail.com>
2021-02-11 23:23:24 +0000

    git-buildpackage: update 0.9.22 bottle.

--- Formula/git-buildpackage.rb
+++ Formula/git-buildpackage.rb
@@ -10,10 +10,10 @@ class GitBuildpackage < Formula
   head "https://github.com/agx/git-buildpackage.git"
 
   bottle do
-    root_url "https://github.com/dawidd6/homebrew-tap/releases/download/git-buildpackage-0.9.21"
-    sha256 cellar: :any_skip_relocation, big_sur:      "585a9d79db56917f5a98b608c8bf3eb228ae1c0b9f281add9a53a9c61f969b1d"
-    sha256 cellar: :any_skip_relocation, catalina:     "365b67fe4e104e39e3e7fe23138e762f0ff1914f8700d7e64de22e84e0eceb59"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5075abecf382ec90ef757bf64901681e2bc578fe517ff8ad504fb68cfd88e056"
+    root_url "https://github.com/dawidd6/homebrew-tap/releases/download/git-buildpackage-0.9.22"
+    sha256 cellar: :any_skip_relocation, big_sur:      "b4556ece31ddcafc0ca02c5fc46b8a664519466b824ea13fac2014cb30f837f2"
+    sha256 cellar: :any_skip_relocation, catalina:     "0ed0afddcfe72b2a8ca4281d5d272620c820fd2b86b844cc85195381640c06dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8ac24c65dd2ab0495ecbae09cd6e820bb6923627a485e11b8198810d9364361b"
   end
 
   depends_on "pristine-tar"
