7bbfd5c21329425cc9c002c93bff38c1d0561893
Dawid Dziurla <dawidd0811@gmail.com>
2021-03-02 00:20:34 +0000

    gothanks 0.4.0

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -1,10 +1,9 @@
 class Gothanks < Formula
   desc "Automatically star your go.mod Github dependencies"
   homepage "https://github.com/psampaz/gothanks"
-  url "https://github.com/psampaz/gothanks/archive/v0.3.0.tar.gz"
-  sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
+  url "https://github.com/psampaz/gothanks/archive/v0.4.0.tar.gz"
+  sha256 "300b705751a43f6ae25df57d6c9a5b0c859e92d61fa83cf894329ea9662525f2"
   license "MIT"
-  revision 2
 
   bottle do
     root_url "https://github.com/dawidd6/homebrew-tap/releases/download/gothanks-0.3.0_2"
