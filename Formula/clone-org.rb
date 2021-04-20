32fbcc134d3cf91f6dce26cb4c453214f3d8114e
Dawid Dziurla <dawidd0811@gmail.com>
2021-04-20 12:04:50 +0000

    clone-org 1.3.0

--- Formula/clone-org.rb
+++ Formula/clone-org.rb
@@ -1,10 +1,9 @@
 class CloneOrg < Formula
   desc "Clone all repos of a github organization"
   homepage "https://github.com/caarlos0/clone-org"
-  url "https://github.com/caarlos0/clone-org/archive/v1.2.0.tar.gz"
-  sha256 "930c1c71da669d45a9e8e006a1f2d5782c268bc35443bb7e648b7d1690936026"
+  url "https://github.com/caarlos0/clone-org/archive/v1.3.0.tar.gz"
+  sha256 "41b946e01160fe0c6b34ac7cf7d800bbd2b8fdc485e52019f2558382e1d4c66d"
   license "MIT"
-  revision 1
   head "https://github.com/caarlos0/clone-org.git"
 
   bottle do
