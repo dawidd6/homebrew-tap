6d72063fbe9f14d4567118f2aa92edbe25c15ae3
Dawid Dziurla <dawidd0811@gmail.com>
2021-08-11 21:37:49 +0000

    clone-org: update 1.4.0 bottle.

--- Formula/clone-org.rb
+++ Formula/clone-org.rb
@@ -7,10 +7,10 @@ class CloneOrg < Formula
   head "https://github.com/caarlos0/clone-org.git"
 
   bottle do
-    root_url "https://github.com/dawidd6/homebrew-tap/releases/download/clone-org-1.2.0_1"
-    sha256 cellar: :any_skip_relocation, big_sur:      "d2dc34d6456cda1da0f9fbdacd472d6eeb0106724719f6c7ccf1e5649382ac29"
-    sha256 cellar: :any_skip_relocation, catalina:     "d524cbb1b09eccf3a6fa927389a851a82c961b90185a7daebf7c693c8192747d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "58c92a0c39ef217a20db43df0c263b522250d08b18023ceb536d0b10737b96a4"
+    root_url "https://github.com/dawidd6/homebrew-tap/releases/download/clone-org-1.4.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "7d1aeb2f1fec294ce49e895368be08fe6a488f9003fa22350c50c0ac3c778f9a"
+    sha256 cellar: :any_skip_relocation, catalina:     "129aca82085295dfc44cf458713ca55ebf9575c7473b92c7c87a78cae699ade0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "068eff64d267d27b799aef10dd9d19755f547763f76747e8a0499f42b6858829"
   end
 
   depends_on "go" => :build
