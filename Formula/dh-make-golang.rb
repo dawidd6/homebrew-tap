e4b9c4f35f56aec58ab80ec5a37f7c26c6fe3071
Dawid Dziurla <dawidd0811@gmail.com>
2021-08-28 08:54:11 +0000

    dh-make-golang: update 0.5.0 bottle.

--- Formula/dh-make-golang.rb
+++ Formula/dh-make-golang.rb
@@ -9,10 +9,10 @@ class DhMakeGolang < Formula
   head "https://github.com/Debian/dh-make-golang.git"
 
   bottle do
-    root_url "https://github.com/dawidd6/homebrew-tap/releases/download/dh-make-golang-0.4.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "d0134dd842ae3addc2a23cd6eb4907033e63e03ccdf2b87d08d8cc56642c4c25"
-    sha256 cellar: :any_skip_relocation, catalina:     "5198eb6526ae0b81d3bd62bc3e482b20f7db66b6fd559a8d2bc01aaf948d0ead"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7d15fbffa0241b110ef9923d3f23db75b2f369d620a6da69b581d302484a9fbe"
+    root_url "https://github.com/dawidd6/homebrew-tap/releases/download/dh-make-golang-0.5.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "569f75525f9da6195733c2560f61caf33d0169d9cc56736bbab4ef58738b64a5"
+    sha256 cellar: :any_skip_relocation, catalina:     "5865edecc64cff59f0fdee07e09f10db0c9e36fd311c6438060e070461bd0819"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "573fc2adba8eb2bde7789469139983c551bc600e5c4743c939d16ca5d4107f66"
   end
 
   depends_on "go" => :build
