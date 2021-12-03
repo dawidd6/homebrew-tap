8c8d52b19da6e8fcb38a6ca069b8528cba63f2d7
Dawid Dziurla <dawidd0811@gmail.com>
2021-12-03 06:14:40 +0000

    go@1 1.17.4
    Closes #245.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/go@1.rb
+++ Formula/go@1.rb
@@ -1,8 +1,8 @@
 class GoAT1 < Formula
   desc "Go compiler 1.x (includes beta, alpha and rc releases)"
   homepage "https://golang.org"
-  url "https://dl.google.com/go/go1.17.3.src.tar.gz"
-  sha256 "705c64251e5b25d5d55ede1039c6aa22bea40a7a931d14c370339853643c3df0"
+  url "https://dl.google.com/go/go1.17.4.src.tar.gz"
+  sha256 "4bef3699381ef09e075628504187416565d710660fec65b057edf1ceb187fc4b"
   license "BSD-3-Clause"
 
   livecheck do
