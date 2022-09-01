447e3eee2b346d1088997f71c5165429153f6950
Dawid Dziurla <dawidd0811@gmail.com>
2022-09-01 09:59:34 +0200

    distrobox 1.3.1 (new formula)
    * distrobox 1.3.1 (new formula)
    * distrobox: update
    * distrobox: set HEAD branch
    
    Closes #266.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ Formula/distrobox.rb
@@ -0,0 +1,18 @@
+class Distrobox < Formula
+  desc "Use any linux distribution inside your terminal"
+  homepage "https://distrobox.privatedns.org/"
+  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.3.1.tar.gz"
+  sha256 "22b6625ca243f55c08630d37015cdbfbe1939516022bfef502aa6603f42b4d00"
+  license "GPL-3.0-only"
+  head "https://github.com/89luca89/distrobox.git", branch: "main"
+
+  depends_on :linux
+
+  def install
+    system "./install", "--prefix", prefix
+  end
+
+  test do
+    system bin/"distrobox-create", "--dry-run"
+  end
+end
