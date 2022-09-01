58c1814656c204ecc09df537e36ea9387e796a9a
Dawid Dziurla <dawidd0811@gmail.com>
2022-09-01 10:17:59 +0200

    distrobox: update

--- Formula/distrobox.rb
+++ Formula/distrobox.rb
@@ -3,13 +3,13 @@ class Distrobox < Formula
   homepage "https://distrobox.privatedns.org/"
   url "https://github.com/89luca89/distrobox/archive/refs/tags/1.3.1.tar.gz"
   sha256 "22b6625ca243f55c08630d37015cdbfbe1939516022bfef502aa6603f42b4d00"
-  license "GPL-3.0"
+  license "GPL-3.0-only"
   head "https://github.com/89luca89/distrobox.git"
 
   depends_on :linux
 
   def install
-    system "./install.sh", "--prefix", prefix
+    system "./install", "--prefix", prefix
   end
 
   test do
