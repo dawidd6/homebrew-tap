5faee13c05416572b972f770292a29d90042066a
Dawid Dziurla <dawidd0811@gmail.com>
2022-09-15 14:43:44 +0000

    sshpass: add 1.09 bottle.

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -5,6 +5,13 @@ class Sshpass < Formula
   sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/dawidd6/homebrew-tap/releases/download/sshpass-1.09"
+    sha256 cellar: :any_skip_relocation, monterey:     "60f59752799a65593b48d052d9fc629ba25ba60fb12d0175f04afc1e8b6ffd4f"
+    sha256 cellar: :any_skip_relocation, big_sur:      "c2bbe08d8a200c840c34697a6728c1f1a098f1fdc7b131dd31910151ddeb15b6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "67482d6afccf9b1cb17dd39ee9d9bae7deb1f3ad53e3ca81ae1882311b006f98"
+  end
+
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make", "install"
