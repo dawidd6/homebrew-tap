f27f08ba02d6409e88de23f8eb61fad2d83a0d35
Dawid Dziurla <dawidd0811@gmail.com>
2022-09-30 06:45:07 +0000

    podman: add 4.2.1 bottle.

--- Formula/podman.rb
+++ Formula/podman.rb
@@ -5,6 +5,11 @@ class Podman < Formula
   sha256 "b10004e91a9f5528da450466ec8e6f623eaa28ada79e3044c238895b2c8d1df3"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/dawidd6/homebrew-tap/releases/download/podman-4.2.1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "871b99aa71ea5f264dae3fa416b07a285c32128688791848e2aac58ab41b9fff"
+  end
+
   depends_on "go" => :build
   depends_on "go-md2man" => :build
   depends_on "pkg-config" => :build
