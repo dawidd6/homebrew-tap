606c6773d59876de4457544b40e70eaa55efc679
Dawid Dziurla <dawidd0811@gmail.com>
2022-10-02 19:40:46 +0200

    podman: fixes
    Closes #276.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/podman.rb
+++ Formula/podman.rb
@@ -11,9 +11,12 @@ class Podman < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "96239403e91973a70ec9a6fc547273ca985ec5fa1ae184d4925cab4e7b96f8ce"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "go" => :build
   depends_on "go-md2man" => :build
   depends_on "pkg-config" => :build
+  depends_on "rust" => :build
   depends_on "conmon"
   depends_on "crun"
   depends_on "fuse-overlayfs"
@@ -23,13 +26,62 @@ class Podman < Formula
   depends_on "slirp4netns"
   depends_on "systemd"
 
+  resource "storage.conf" do
+    url "https://src.fedoraproject.org/rpms/containers-common/raw/23d18a07d33c4db77905a38a77278c7a499ee097/f/storage.conf"
+    sha256 "c426db62a5dd5e5af54f3b4608cbc4da2ef682f0a6b02d9cb9000c1231df48dc"
+  end
+
+  resource "catatonit" do
+    url "https://github.com/openSUSE/catatonit/archive/refs/tags/v0.1.7.tar.gz"
+    sha256 "e22bc72ebc23762dad8f5d2ed9d5ab1aaad567bdd54422f1d1da775277a93296"
+
+    # Fix autogen.sh. Delete on next release.
+    patch do
+      url "https://github.com/openSUSE/catatonit/commit/99bb9048f532257f3a2c3856cfa19fe957ab6cec.patch?full_index=1"
+      sha256 "cc0828569e930ae648e53b647a7d779b1363bbb9dcbd8852eb1cd02279cdbe6c"
+    end
+  end
+
+  resource "netavark" do
+    url "https://github.com/containers/netavark/archive/refs/tags/v1.2.0.tar.gz"
+    sha256 "35b710197f321a2e45c59460fd8faf67b7b8ebc345d22aa8ecccf806790c6edc"
+  end
+
+  resource "aardvark-dns" do
+    url "https://github.com/containers/aardvark-dns/archive/refs/tags/v1.2.0.tar.gz"
+    sha256 "434163027660feebb87e288d9c9f8468a1a9d1a632d1f9fe0a84585dfde3f4dd"
+  end
+
+  # Patch to fix "podman run --rm" hanging on stopping the container.
+  patch :DATA
+
   def install
     ENV.O0
     ENV["PREFIX"] = prefix
+    ENV["EXTRA_LDFLAGS"] = %W[
+      -X github.com/containers/storage/types.defaultConfigFile=#{etc}/containers/storage.conf
+      -X github.com/containers/image/v5/signature.systemDefaultPolicyPath=#{etc}/containers/policy.json
+      -X github.com/containers/common/pkg/config.additionalHelperBinariesDir=#{libexec}/podman
+      -X github.com/containers/common/pkg/config.additionalConmonEnvPath=#{HOMEBREW_PREFIX}/bin
+    ].join(" ")
     system "make", "podman", "podman-remote", "rootlessport", "docs"
     system "make", "install.bin", "install.remote", "install.man", "install.completions"
     (etc/"containers").install "test/policy.json"
-    (etc/"containers").install "test/registries.conf"
+    (etc/"containers").install resource("storage.conf")
+    resource("catatonit").stage do
+      system "./autogen.sh"
+      system "./configure"
+      system "make"
+      mv "catatonit", libexec/"podman/"
+    end
+    resource("netavark").stage do
+      system "cargo", "install", *std_cargo_args
+      mv bin/"netavark", libexec/"podman/"
+    end
+    resource("aardvark-dns").stage do
+      system "cargo", "install", *std_cargo_args
+      mv bin/"aardvark-dns", libexec/"podman/"
+    end
   end
 
   def caveats
@@ -38,9 +90,63 @@ class Podman < Formula
     EOS
   end
 
+  service do
+    run [opt_bin/"podman", "system", "service", "--time=0"]
+    environment_variables PATH: std_service_path_env
+    working_dir HOMEBREW_PREFIX
+  end
+
   test do
-    out = shell_output("#{bin}/podman run --rm docker.io/library/alpine true 2>&1", 125)
-    assert_match "is not a shared mount", out
-    assert_match "this could cause issues or missing mounts with rootless containers", out
+    # It checks if there are no helper binaries in bindir.
+    assert_equal %W[
+      #{bin}/podman
+      #{bin}/podman-remote
+    ].sort, Dir[bin/"*"].sort
+    # It checks if all needed helper binaries are present.
+    assert_equal %W[
+      #{libexec}/podman/catatonit
+      #{libexec}/podman/netavark
+      #{libexec}/podman/aardvark-dns
+      #{libexec}/podman/rootlessport
+    ].sort, Dir[libexec/"podman/*"].sort
+    # It checks if podman-remote binary could be executed.
+    out = shell_output("#{bin}/podman-remote info 2>&1", 125)
+    assert_match "Cannot connect to Podman", out
+    # Catatonit is an init program run in container.
+    # It checks if catatonit is statically linked.
+    out = shell_output("file #{libexec}/podman/catatonit")
+    assert_match "statically linked", out
+    # It checks configured network backend and storage driver.
+    out = shell_output("#{bin}/podman system info")
+    assert_match "networkBackend: netavark", out
+    assert_match "graphDriverName: overlay", out
+    # Pull the image earlier so we won't measure the network bandwidth below too.
+    # It checks if policy.json file is properly read.
+    system bin/"podman", "pull", "docker.io/library/alpine"
+    # Below command should be fast.
+    # It checks if "podman run --rm" patch is still working.
+    system "timeout", "5", bin/"podman", "run", "--rm", "docker.io/library/alpine", "true"
   end
 end
+__END__
+diff --git a/vendor/github.com/containers/common/pkg/config/default.go b/vendor/github.com/containers/common/pkg/config/default.go
+index 161a9c8d6..003a3f34e 100644
+--- a/vendor/github.com/containers/common/pkg/config/default.go
++++ b/vendor/github.com/containers/common/pkg/config/default.go
+@@ -103,6 +103,7 @@ var (
+ 	// should be set during link-time, if different packagers put their
+ 	// helper binary in a different location.
+ 	additionalHelperBinariesDir string
++	additionalConmonEnvPath string
+ )
+ 
+ // nolint:unparam
+@@ -359,7 +360,7 @@ func defaultConfigFromMemory() (*EngineConfig, error) {
+ 	c.OCIRuntime = c.findRuntime()
+ 
+ 	c.ConmonEnvVars = []string{
+-		"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
++		fmt.Sprintf("PATH=%s:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", additionalConmonEnvPath),
+ 	}
+ 	c.ConmonPath = []string{
+ 		"/usr/libexec/podman/conmon",
